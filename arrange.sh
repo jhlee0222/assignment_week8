d3f2ea40cbf8f3f7f7195115366ea031abbe501a
jhlee0222 <127393055+jhlee0222@users.noreply.github.com>
2023-04-20 19:12:49 +0900

    Update arrange.sh

--- arrange.sh
+++ arrange.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
 #test
+mv ./file/[aA]*.txt ./a/
+mv ./file/[bB]*.txt ./b/
+mv ./file/[cC]*.txt ./c/
+mv ./file/[dD]*.txt ./d/
+mv ./file/[eE]*.txt ./e/
+mv ./file/[fF]*.txt ./f/
+mv ./file/[gG]*.txt ./g/
+mv ./file/[hH]*.txt ./h/
+mv ./file/[iI]*.txt ./i/
+mv ./file/[jJ]*.txt ./j/
+mv ./file/[kK]*.txt ./k/
+mv ./file/[lL]*.txt ./l/
+mv ./file/[mM]*.txt ./m/
+mv ./file/[nN]*.txt ./n/
+mv ./file/[oO]*.txt ./o/
+mv ./file/[pP]*.txt ./p/
+mv ./file/[qQ]*.txt ./q/
+mv ./file/[rR]*.txt ./r/
+mv ./file/[sS]*.txt ./s/
+mv ./file/[tT]*.txt ./t/
+mv ./file/[uU]*.txt ./u/
+mv ./file/[vV]*.txt ./v/
+mv ./file/[wW]*.txt ./w/
+mv ./file/[xX]*.txt ./x/
+mv ./file/[yY]*.txt ./y/
+mv ./file/[zZ]*.txt ./z/
